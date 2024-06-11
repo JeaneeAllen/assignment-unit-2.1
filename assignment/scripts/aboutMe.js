adae78ba8f8356e016d0f361d2408ef242027492
Jeanee Allen <allenjeanee@yahoo.com>
2024-06-11 10:10:47 -0500

    answered q12

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -30,20 +30,30 @@ console.log (adventurous)
 
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
 //     Console log the value of `food`
+let food = "steak, greenbeans, and mashed potatoes"
+console.log (food)
 
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
+let pets = 2
+console.log (pets)
 
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+let friendsPets = 1
 
 // 10 - Add two pets to your `pets` variable
+pets += 2
 
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+const allowedPets = 3
 
 // 12 - Create a variable called `result`. Create a conditional:
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
+let result;
+if (adventurous === 'true') {console.log ('Adventures are great!');}
+else {console.log ('How about we stay home?');}
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
