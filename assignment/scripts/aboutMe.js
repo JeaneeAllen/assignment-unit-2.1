c911994a1f494611e5755fc1a2ac24faa96f6fe5
Jeanee Allen <allenjeanee@yahoo.com>
2024-06-11 09:31:16 -0500

    answered q5-6, and push to check results

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -20,9 +20,13 @@ console.log (luckyNumber)
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
+let introduction = 'My name is ' + fullName + 'and I think ' + luckyNumber + ' is a winner!'
+console.log (introduction)
 
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
+let Adventures = false
+console.log (Adventures)
 
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
 //     Console log the value of `food`
