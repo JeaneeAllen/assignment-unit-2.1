879dddb4e810f7ebb4bde49cae5a0ad208d664e3
Jeanee Allen <allenjeanee@yahoo.com>
2024-06-11 01:38:28 -0500

    updated questions 1-3

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,9 +1,13 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Jen ';
+console.log (firstName)
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Allen'
+console.log (lastName)
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
+let fullName = firstName + lastName
+console.log (fullName)
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
 
